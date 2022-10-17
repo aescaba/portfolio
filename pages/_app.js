8dc6506f7b582144e275873284b42f0f5d88dc79
Amanda Escaba <amandaescaba@gmail.com>
2022-10-16 21:02:36 -0400

    added components folder, initialized navbar and layout from tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+// navbar code from https://j471n.in/blogs/active-navbar-next-js
+
 import '../styles/globals.css'
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+  <Component {...pageProps} />
+  </Layout>
+  );
 }
 
 export default MyApp
