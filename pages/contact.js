0efe32f9ea68403f71e37916241e29e30da1cf77
Amanda Escaba <amandaescaba@gmail.com>
2022-10-17 22:48:02 -0400

    edit contact page buttons

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import PageTitle from '../components/pagetitle'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 export default function Contact() {
   return (
@@ -17,10 +18,19 @@ export default function Contact() {
             <Link href="mailto:amandaescaba@gmail.com">amandaescaba@gmail.com</Link>
         </h3>
 
-        <div class="flex flex-col-reverse lg:flex-row mt-6">
-          <button class="outline outline-2 rounded-full px-12 py-1 hover:bg-white hover:text-black">
+        <div class="flex flex-row mt-6 space-x-4">
+          <button class="outline outline-2 rounded-full px-12 py-1 hover:bg-white hover:text-black font-semibold">
             <a href="https://drive.google.com/file/d/1n4lYfD1M7ZoGZGENIhkVUaDjUM4hhpkh/view?usp=sharing" target="_blank">Resume</a>
           </button>
+
+          <button class="">
+            <a href="https://www.linkedin.com/in/aescaba/" target="_blank"><AiFillLinkedin size={40}/></a>
+          </button>
+
+          <button class="">
+            <a href="https://github.com/aescaba" target="_blank"><AiFillGithub size={40}/></a>
+          </button>
+
         </div>
 
       </div>
