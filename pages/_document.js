e6f2bf6f58b9cac2837746ca2f0ec58a261d952b
Amanda Escaba <amandaescaba@gmail.com>
2022-10-17 23:13:54 -0400

    add overpass google font

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+// https://medium.com/@prodmxle/adding-custom-fonts-to-nextjs-application-with-tailwind-css-894f716eec16
+
+
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+  } from 'next/document'
+  
+  class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+      const initialProps = await Document.getInitialProps(ctx)
+  
+      return initialProps
+    }
+  
+    render() {
+      return (
+        <Html>
+          <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Overpass" rel="stylesheet" />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      )
+    }
+  }
+  
+  export default MyDocument
