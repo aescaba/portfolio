ef882e1c6519ec7bbffb6091c201694210f1ea04
Amanda Escaba <amandaescaba@gmail.com>
2022-11-01 18:35:50 -0400

    spacing and comments

--- pages/about.js
+++ pages/about.js
@@ -8,27 +8,30 @@ export default function About() {
 
       <PageTitle title={"About"}/>
 
-    <div class="flex flex-col-reverse lg:flex-row">
-      <div class="max-w-sm mt-16 lg:mt-0 lg:mr-20 xl:mr-24 place-self-center lg:place-self-auto">
-      <Image class="" src="/../public/images/sunflowers.jpg" alt="photo of me in a sunflower field" width={1536} height={2048}/>
-      </div>
-      <div class="space-y-4">
-        <p class="text-xl">
-        <strong>Hi, I’m Amanda (she/her)!</strong> I'm a proud Filipino American who grew up in the Hudson Valley, New York and
-         spent five years studying computer science and interaction design at Northeastern University in Boston.</p>
+      <div class="flex flex-col-reverse lg:flex-row">
+        {/* first column - photo */}
+        <div class="max-w-sm mt-16 lg:mt-0 lg:mr-20 xl:mr-24 place-self-center lg:place-self-auto">
+          <Image class="" src="/../public/images/sunflowers.jpg" alt="photo of me in a sunflower field" width={1536} height={2048}/>
+        </div>
 
-        <p class="text-xl">I've taken on the responsibilities of a front-end developer and UI/UX designer and I 
-        thrive in positions that incorporate skills from both of my disciplines. My favorite thing that design and
-         code have in common is the creative process—I love seeing how the decisions made each step of the way come 
-         together to support the final product.</p>
+        {/* second column - bio */}
+        <div class="space-y-4">
+          <p class="text-xl">
+          <strong>Hi, I’m Amanda (she/her)!</strong> I'm a proud Filipino American who grew up in the Hudson Valley, New York and
+          spent five years studying computer science and interaction design at Northeastern University in Boston.</p>
 
-        <p class="text-xl">When it comes to my work, I enjoy improving any type of user experience, but I’m most 
-        passionate about sharing and preserving culture and uplifting marginalized voices, especially through storytelling.</p>
+          <p class="text-xl">I've taken on the responsibilities of a front-end developer and UI/UX designer and I 
+          thrive in positions that incorporate skills from both of my disciplines. My favorite thing that design and
+          code have in common is the creative process—I love seeing how the decisions made each step of the way come 
+          together to support the final product.</p>
 
-        <p class="text-xl">I spend my time cooking and baking dishes from my favorite cuisines, ignoring my pile of unread books 
-        to buy new ones, singing both parts of the duet by myself, tinkering with my mechanical keyboard, making too many Spotify
-         playlists, starting more video games than I tend to finish, and getting distracted by cute dogs in public.</p>
-         </div>
+          <p class="text-xl">When it comes to my work, I enjoy improving any type of user experience, but I’m most 
+          passionate about sharing and preserving culture and uplifting marginalized voices, especially through storytelling.</p>
+
+          <p class="text-xl">I spend my time cooking and baking dishes from my favorite cuisines, ignoring my pile of unread books 
+          to buy new ones, singing both parts of the duet by myself, tinkering with my mechanical keyboard, making too many Spotify
+          playlists, starting more video games than I tend to finish, and getting distracted by cute dogs in public.</p>
+        </div>
       </div>
     </div>
   )
