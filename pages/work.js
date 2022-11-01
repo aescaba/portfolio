6f83a32f6108f5d18f748c2d40b4fcc806039813
Amanda Escaba <amandaescaba@gmail.com>
2022-11-01 19:42:53 -0400

    add placeholder image, styling work page

--- pages/work.js
+++ pages/work.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 import PageTitle from '../components/pagetitle'
+import Image from 'next/image'
+
 
 export default function Work() {
   return (
@@ -7,35 +9,35 @@ export default function Work() {
 
       <PageTitle title={"Work"}/>
 
-      <div class="grid grid-cols-2">
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-14 lg:gap-28">
 
         {/* project one */}
         <div class="">
-          <div class="">
-            Photo
+          <div class="mb-4 lg:mb-6">
+            <Image class="" src="/../public/images/work/placeholder_art.png" alt="placeholder" width={1280} height={800}/>
+          </div>
+
+          <div class="font-bold">
+            Project #1 Title
           </div>
 
           <div class="">
-            Title
-          </div>
-
-          <div class="">
-            Description
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           </div>
         </div>
 
         {/* project two */}
         <div class="">
-          <div class="">
-            Photo
+          <div class="mb-4 lg:mb-6">
+            <Image class="" src="/../public/images/work/placeholder_art.png" alt="placeholder" width={1280} height={800}/>
+          </div>
+
+          <div class="font-bold">
+            Project #2 Title
           </div>
 
           <div class="">
-            Title
-          </div>
-
-          <div class="">
-            Description
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           </div>
         </div>
 
