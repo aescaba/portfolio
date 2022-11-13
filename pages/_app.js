48ab25c80daf014545331c2be4598d77018cc995
Amanda Escaba <amandaescaba@gmail.com>
2022-11-13 18:10:25 -0500

    add animation to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 // navbar code from https://j471n.in/blogs/active-navbar-next-js
+// framer motion tutorial from https://javascript.plainenglish.io/how-to-create-page-transitions-in-next-js-with-framer-motion-47642c462c62
 
 import '../styles/globals.css'
+import { motion } from 'framer-motion';
 import Layout from '../components/layout';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
+    <motion.div
+    key={router.route}
+    initial="initial"
+    animate="animate"
+    variants={{
+      initial: {
+        opacity: 0,
+      },
+      animate: {
+        opacity: 1,
+      },
+    }}>
   <Layout>
   <Component {...pageProps} />
   </Layout>
+  </motion.div>
   );
 }
 
