9988322ad116c782806b4eb9fe4e56d9df6eafc8
Amanda Escaba <amandaescaba@gmail.com>
2023-01-13 00:05:25 -0500

    edit div nesting for work listings

--- components/worklisting.js
+++ components/worklisting.js
@@ -3,19 +3,20 @@ import Link from 'next/link';
 
 export default function WorkListing({imgsrc, path, title, description}) {
     return (
-        <div class="grayscale-[50%] hover:filter-none hover:text-amber-400">
-            <Link href={"/work/"+ `${path}`}>
-                <a>
-                    <div class="mb-4 lg:mb-6">
-                        <Image alt={"Preview of " + `${title}`} src={imgsrc} width={1280} height={800}/>
-                    </div>
+        <div class="">
+            <div class="grayscale-[50%] hover:filter-none hover:text-amber-400">
+                <Link href={"/work/"+ `${path}`}>
+                    <a>
+                        <div class="mb-4 lg:mb-6">
+                            <Image alt={"Preview of " + `${title}`} src={imgsrc} width={1280} height={800}/>
+                        </div>
 
-                    <div class="font-bold text-xl">
-                        {title}
-                    </div>
-                </a>
-            </Link>
-
+                        <div class="font-bold text-xl">
+                            {title}
+                        </div>
+                    </a>
+                </Link>  
+            </div>
             <div class="font-light text-neutral-300 text-lg leading-6">
                 {description}
             </div>
