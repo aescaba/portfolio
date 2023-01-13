ac494eabf81fb245c58d5ded95ea0f060068395f
Amanda Escaba <amandaescaba@gmail.com>
2023-01-13 15:10:37 -0500

    testing transitions and animation structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 // navbar code from https://j471n.in/blogs/active-navbar-next-js
+// animation https://letsbuildui.dev/articles/animated-page-transitions-in-nextjs
 
 import '../styles/globals.css'
 import Layout from '../components/layout';
+import Transition from '../components/transition';
 
 
 function MyApp({ Component, pageProps, }) {
   return (
 
     <Layout>
-      <Component {...pageProps} />
+      {/* <Transition> */}
+        <Component {...pageProps} />
+      {/* </Transition> */}
     </Layout>
   );
 }
