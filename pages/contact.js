39b7ede6fb58fbc96b030fd3f38e4dd07824a74f
Amanda Escaba <amandaescaba@gmail.com>
2023-01-13 15:32:46 -0500

    transition delay on resume button

--- pages/contact.js
+++ pages/contact.js
@@ -19,11 +19,13 @@ export default function Contact() {
         </h3>
 
         <div class="flex flex-row mt-6 space-x-4">
-          <button class="outline outline-2 rounded-full px-14 py-1 hover:bg-white hover:text-black text-xl font-bold hover:bg-gradient-to-l from-orange-300 via-amber-300 to-blue-400">
+          <a href="https://drive.google.com/file/d/1n4lYfD1M7ZoGZGENIhkVUaDjUM4hhpkh/view?usp=sharing" target="_blank" rel="noreferrer">
+          <button class="outline outline-2 rounded-full px-14 py-2 transition ease-in-out delay-[25ms] hover:bg-white hover:text-black hover:outline-0 text-xl font-bold hover:bg-gradient-to-l from-orange-300 via-amber-300 to-blue-400">
           {/* <button class="outline outline-2 rounded-full px-14 py-1 text-xl text-white font-bold bg-gradient-to-r hover:bg-gradient-to-l from-blue-500 via-orange-500 to-amber-400"> */}
           {/* <button class="outline outline-2 rounded-full px-14 py-1 text-xl text-black font-black bg-gradient-to-r hover:bg-gradient-to-l from-blue-300 via-amber-200 to-orange-200"> */}
-            <a href="https://drive.google.com/file/d/1n4lYfD1M7ZoGZGENIhkVUaDjUM4hhpkh/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+            <p>Resume</p>
           </button>
+          </a>
 
           <button class="opacity-75 hover:opacity-100">
             <a href="https://www.linkedin.com/in/aescaba/" target="_blank" rel="noreferrer"><AiFillLinkedin size={45}/></a>
