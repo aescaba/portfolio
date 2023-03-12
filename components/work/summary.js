eabd00899af41fdcfb373974f38abac706558744
Amanda Escaba <amandaescaba@gmail.com>
2023-03-11 22:08:44 -0500

    map tools list

--- components/work/summary.js
+++ components/work/summary.js
@@ -1,34 +1,35 @@
 import Image from "next/image";
 
-export default function Summary({summary, toolsList, imgInfo }) {
+export default function Summary({ summary, toolsList, imgInfo }) {
 
-// imgInfo: src, alt, width, height
+    // imgInfo: src, alt, width, height
 
-const src = `${imgInfo.src}`;
-const alt = `${imgInfo.alt}`;
-const width = `${imgInfo.width}`;
-const height = `${imgInfo.height}`;
+    const src = `${imgInfo.src}`;
+    const alt = `${imgInfo.alt}`;
+    const width = `${imgInfo.width}`;
+    const height = `${imgInfo.height}`;
 
     return (
         <div class="flex flex-col lg:flex-row lg:gap-x-8 mb-20">
-      {/* first column - summary */}
-        <div class="mb-8 lg:mb-4 lg:basis-1/2">
-            <p class="text-[1.75rem] leading-9 font-thin">{summary}</p>
+            {/* first column - summary */}
+            <div class="mb-8 lg:mb-4 lg:basis-1/2">
+                <p class="text-[1.75rem] leading-9 font-thin">{summary}</p>
 
-            <h3 class="text-xl mt-10 py-2 font-semibold">Skills/Tools</h3>
-            <div class="flex space-x-6 text-lg">
-                <p>{toolsList}</p>
-                <p>Adobe InDesign</p>
-                <p>Adobe XD</p>
-                <p>Procreate</p>
+                <h3 class="text-xl mt-10 py-2 font-semibold">Skills/Tools</h3>
+                <div class="flex space-x-6 text-lg">
+                    {toolsList.map((tool) => {
+                        return (
+                            <p>{tool}</p>
+                        );
+                    })}
+                </div>
             </div>
+
+            {/* second column - image */}
+            <div class="lg:basis-1/2">
+                <Image class="" src={src} alt={alt} width={width} height={height} />
+            </div>
+
         </div>
-
-      {/* second column - image */}
-          <div class="lg:basis-1/2">
-            <Image class="" src={src} alt={alt} width={width} height={height}/>
-          </div>
-
-      </div>
     );
 }
