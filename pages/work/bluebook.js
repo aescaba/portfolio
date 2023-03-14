7781ad2513c9dc26820fe0cf0b74691b71647a8f
Amanda Escaba <amandaescaba@gmail.com>
2023-03-14 19:02:10 -0400

    update blue book work page to new layout

--- pages/work/bluebook.js
+++ pages/work/bluebook.js
@@ -1,41 +1,53 @@
 import PageTitle from '../../components/pagetitle'
+import Image from 'next/image';
+import Summary from '../../components/work/summary';
+import Overview from '../../components/work/overview';
+
+const summary = "Prioritizing the user experience is always a driving factor of mine—so when I was faced with the challenge of designing a microsite for the Massachusetts Bay Transportation Authority Blue Book, I was excited to put my technical, design, and organization skills to use to make navigating through large amounts of data more bearable."
+
+const toolsList = [
+  "Persona development",
+  "Sketching & Wireframing",
+  "Adobe XD"
+]
+
+const imgInfo = {
+  src: "/images/work/mbta-bluebook/wireframes_3_page-0003.jpg",
+  alt: "Screenshot of a wireframed webpage for MBTA Blue Book microsite",
+  width: "4000",
+  height: "2932"
+}
+
+const overview = [
+  "The MBTA has published several editions of the MBTA Ridership and Service Statistics, also known as the “Blue Book”, since 1988. The Blue Book contains a collection of MBTA data such as ridership, bus speeds, track distances, fleet rosters, and more, but its updates have been inconsistent and its lengthy PDF format was inconvenient to navigate.",
+  "One of my first projects at MassDOT/MBTA’s Office of Performance Management and Innovation, another co-op and I worked on converting the PDF into a website. While he worked on the data organization aspect of the project, I led the redesign process of transforming the Blue Book PDF into an interactive website that was easy to navigate for its main userbase."
+]
+
+const responsibilities = [
+  "Identified four groups of target audiences and developed user personas",
+  "Led design efforts and technical research as sole designer and one of two developers on team",
+  "Designed potential screens and flow of microsite with sketches and interactive wireframes",
+  "Analyzed technical requirements for hosting desired microsite versus available hosting environment and capabilities"
+]
 
 export default function BlueBook() {
   return (
     <div class="font-light text-lg leading-6 pb-24">
-      <PageTitle title={"MBTA Blue Book Redesign"}/>
 
-      <div class="mb-14"> 
-      {/* insert photo */}
-      <p class="text-2xl my-10 font-medium text-blue-300 italic">preview coming soon</p>
+      <PageTitle title={"MBTA Blue Book Redesign"} />
 
-        <div class="mb-4">
-          <h2 class="text-2xl py-2 font-semibold">Overview</h2>
-          <p>The MBTA has published several editions of the MBTA Ridership and Service Statistics, also known as the “Blue Book”, since 1988. The Blue Book contains a collection of MBTA data such as ridership, bus speeds, track distances, fleet rosters, and more, but its updates have been inconsistent and its lengthy PDF format was inconvenient to navigate.
-          </p>
-          <p class='mt-2'>One of my first projects at MassDOT/MBTA&apos;s Office of Performance Management and Innovation, another co-op and I worked on converting the PDF into a website. While he worked on the data organization aspect of the project, I led the redesign process of transforming the Blue Book PDF into an interactive website that was easy to navigate for its main userbase.</p>
-        </div>
+      {/* first row - summary and preview photo */}
+      <Summary
+        summary={summary}
+        toolsList={toolsList}
+        imgInfo={imgInfo}
+      />
 
-        {/* <div>
-          <h3 class="text-xl py-2 font-semibold">Skills/Tools</h3>
-          <div class="flex space-x-6">
-            <p>Skill One</p>
-            <p>Skill Two</p>
-            <p>Another Skill</p>
-          </div>
-        </div> */}
-      </div>
-
-      <div class="mb-4">
-          <h2 class="text-xl py-2 font-semibold">Responsibilities</h2>
-          <ul class="list-disc list-outside ml-6">
-            <li>Developed three distinct user personas to focus design efforts around</li>
-            <li>Researched and collected input on areas of interest in Greater Boston neighborhoods relevant to university student/young professional interests and habits</li>
-            <li>Sketched maps of different scopes to incorporate students&apos; locations, common travel routes, and places of interest</li>
-            <li>Coordinated meetings and analyzed group findings and discussions to achieve project goals</li>
-            <li>Assembled portfolio of process, sketches, and final drafts to be saved for potential future use</li>
-          </ul>
-        </div>
+      {/* second row - overview and responsibilities */}
+      <Overview
+        overview={overview}
+        responsibilities={responsibilities}
+      />
 
     </div>
   );
