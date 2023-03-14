2d5c8f06fafdfe3201326d1e49c793a209a7859c
Amanda Escaba <amandaescaba@gmail.com>
2023-03-14 00:58:50 -0400

    add wips portfolio and gallerify

--- pages/work.js
+++ pages/work.js
@@ -1,49 +1,75 @@
 import PageTitle from '../components/pagetitle'
 import WorkListing from '../components/worklisting'
-
+import Image from 'next/image'
 
 export default function Work() {
   return (
     <div class="">
 
-      <PageTitle title={"Work"}/>
+      <PageTitle title={"Work"} />
 
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-14 lg:gap-24 pb-24">
 
         {/* CHANGE THIS TO BE A FEED LATER ? */}
 
         {/* project one */}
-          <WorkListing
-            imgsrc={"/images/work/wbecnydmv/wbec-thumb.jpg"}
-            path={"wbecnydmv"}
-            title={"WBEC Metro NY & Greater DMV"}
-            description={"Complete website overhaul to reflect new branding and improve user experience"}>
-          </WorkListing>
+        <WorkListing
+          imgsrc={"/images/work/wbecnydmv/wbec-thumb.jpg"}
+          path={"wbecnydmv"}
+          title={"WBEC Metro NY & Greater DMV"}
+          description={"Complete website overhaul to reflect new branding and improve user experience"}>
+        </WorkListing>
 
         {/* project two */}
         <WorkListing
-            imgsrc={"/images/work/capstone/capstone-thumb.jpg"}
-            path={"capstone"}
-            title={"Tastes of the Motherland"}
-            description={"Northeastern University capstone project: an interactive website prototype"}>
+          imgsrc={"/images/work/capstone/capstone-thumb.jpg"}
+          path={"capstone"}
+          title={"Tastes of the Motherland"}
+          description={"Northeastern University capstone project: an interactive website prototype"}>
         </WorkListing>
 
         {/* project three */}
         <WorkListing
-            imgsrc={"/images/work/mbta-unimaps/mbta-unimaps-thumb.jpg"}
-            path={"unimaps"}
-            title={"MBTA University Marketing: Interest-Based Maps"}
-            description={"Outreach plan to promote MBTA ridership to university students and young professionals"}>
+          imgsrc={"/images/work/mbta-unimaps/mbta-unimaps-thumb.jpg"}
+          path={"unimaps"}
+          title={"MBTA University Marketing: Interest-Based Maps"}
+          description={"Outreach plan to promote MBTA ridership to university students and young professionals"}>
         </WorkListing>
 
         {/* project four */}
         <WorkListing
-            imgsrc={"/images/work/mbta-bluebook/mbta-bluebook-thumb.jpg"}
-            path={"bluebook"}
-            title={"MBTA Blue Book Redesign"}
-            description={"Microsite design to improve readability of and broaden access to MBTA data pool"}>
+          imgsrc={"/images/work/mbta-bluebook/mbta-bluebook-thumb.jpg"}
+          path={"bluebook"}
+          title={"MBTA Blue Book Redesign"}
+          description={"Microsite design to improve readability of and broaden access to MBTA data pool"}>
         </WorkListing>
 
+      </div>
+
+      <h2 class="text-2xl mb-12 font-medium">In Progress &mdash; Coming Soon!</h2>
+
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-14 lg:gap-24 pb-24">
+        {/* portfolio preview */}
+        <div class="">
+          <div class="mb-4 lg:mb-6">
+            <Image alt="Blurred photo preview of portfolio project asset" src="/images/work/portfolio-preview-thumb.jpg" width={1280} height={800} />
+          </div>
+
+          <div class="font-bold text-xl">
+            Portfolio Website
+          </div>
+        </div>
+
+                {/* gallerify preview */}
+                <div class="">
+          <div class="mb-4 lg:mb-6">
+            <Image alt="Blurred photo preview of Gallerify project asset" src="/images/work/gallerify-preview-thumb.jpg" width={1280} height={800} />
+          </div>
+
+          <div class="font-bold text-xl">
+            Gallerify
+          </div>
+        </div>
       </div>
 
     </div>
