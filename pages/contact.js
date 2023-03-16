f76688b31b3bb600bfe58c806c34d3f3954d9b10
Amanda Escaba <amandaescaba@gmail.com>
2023-03-15 23:10:52 -0400

    formatting/cleaning comments

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,8 @@ export default function Contact() {
   return (
     <div>
 
-      <PageTitle title={"Contact"}/>
+      <PageTitle title={"Contact"} />
+      
       <div>
 
         <h2 class="text-3xl font-semibold">
@@ -15,24 +16,22 @@ export default function Contact() {
         </h2>
 
         <h3 class="text-2xl transition ease-in-out delay-75 hover:text-amber-400">
-            <Link href="mailto:amandaescaba@gmail.com">amandaescaba@gmail.com</Link>
+          <Link href="mailto:amandaescaba@gmail.com">amandaescaba@gmail.com</Link>
         </h3>
 
         <div class="flex flex-row mt-6 space-x-4">
           <a href="https://drive.google.com/file/d/1n4lYfD1M7ZoGZGENIhkVUaDjUM4hhpkh/view?usp=sharing" target="_blank" rel="noreferrer">
-          <button class="outline outline-2 rounded-full px-8 py-2 transition ease-in-out delay-[25ms] hover:bg-white hover:text-black hover:outline-0 text-xl font-semibold hover:bg-gradient-to-l from-orange-300 via-amber-300 to-blue-400">
-          {/* <button class="outline outline-2 rounded-full px-14 py-1 text-xl text-white font-bold bg-gradient-to-r hover:bg-gradient-to-l from-blue-500 via-orange-500 to-amber-400"> */}
-          {/* <button class="outline outline-2 rounded-full px-14 py-1 text-xl text-black font-black bg-gradient-to-r hover:bg-gradient-to-l from-blue-300 via-amber-200 to-orange-200"> */}
-            <p>Resume</p>
-          </button>
+            <button class="outline outline-2 rounded-full px-8 py-2 transition ease-in-out delay-[25ms] hover:bg-white hover:text-black hover:outline-0 text-xl font-semibold hover:bg-gradient-to-l from-orange-300 via-amber-300 to-blue-400">
+              <p>Resume</p>
+            </button>
           </a>
 
           <button class="saturate-0 hover:saturate-100 transition ease-in-out delay-[25ms]">
-            <a href="https://www.linkedin.com/in/aescaba/" target="_blank" rel="noreferrer"><AiFillLinkedin fill="#fbbf24" size={45}/></a>
+            <a href="https://www.linkedin.com/in/aescaba/" target="_blank" rel="noreferrer"><AiFillLinkedin fill="#fbbf24" size={45} /></a>
           </button>
 
           <button class="saturate-0 hover:saturate-100 transition ease-in-out delay-[25ms]">
-            <a href="https://github.com/aescaba" target="_blank" rel="noreferrer"><AiFillGithub fill="#fbbf24" size={45}/></a>
+            <a href="https://github.com/aescaba" target="_blank" rel="noreferrer"><AiFillGithub fill="#fbbf24" size={45} /></a>
           </button>
 
         </div>
