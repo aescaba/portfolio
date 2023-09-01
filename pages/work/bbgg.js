3857f4b63eb14eefbc8130a66f54e567999fd1b5
Amanda Escaba <amandaescaba@gmail.com>
2023-08-31 22:06:48 -0400

    create bbgg work page - fill with copy

---
+++ pages/work/bbgg.js
@@ -0,0 +1,138 @@
+// library imports
+import Image from 'next/image';
+import { HiOutlineExternalLink } from 'react-icons/hi'
+
+// component imports
+import WorkPage from '../../components/work/workpage';
+import WorkSection from '../../components/work/worksection';
+import WorkRow from '../../components/work/workrow';
+import Summary from '../../components/work/summary';
+import Overview from '../../components/work/overview';
+
+// image imports
+import summaryPhoto from '../../public/images/work/wbecnydmv/WBEC-3-crop-2.png'
+
+const summary = "As a Mid-Level Web Designer and Developer at BBG&G Integrated Marketing, I am an integral member of the web/creative team and work on all aspects of website design and development."
+
+const toolsList = [
+    "HTML/CSS/JavaScript",
+    "Figma",
+    "WordPress",
+    "Search Engine Optimization (SEO)",
+    "Adobe Creative Suite"
+]
+
+const imgInfo = {
+    summaryPhoto,
+    alt: "Screenshot of 'About Us' page on WBEC Metro NY and Greater DMV website",
+    width: "1280",
+    height: "800"
+}
+
+const overview = [
+    "I joined BBG&G as a Junior Web Developer and Designer in late February 2022 and was promoted to Mid-Level in November that same year. I take on various responsibilities as one of two members of the web/creative team, but I mainly play a key part in the design and development of websites.",
+    "Working for a marketing agency with multiple clients, I am constantly juggling a wide range of projects. This role demands adaptability and versatility, but the ability to switch between coding and design tasks swiftly allows me to meet tight deadlines and ensure deliverables are of the highest quality."
+]
+
+const responsibilities = [
+    "Create wireframes, design comps, and prototypes; build templates and manage themes and plugins",
+    "Build sites with WordPress/HTML/CSS/JS/PHP; implement responsive design, SEO optimization, ADA compliance",
+    "Document technical and creative processes in detail; develop training manuals & brand guidelines; lead web training sessions",
+    "Programming, testing, and routine maintenance of client websites",
+    "Collaborate with creative & account teams to support website development; make design & technical decisions best suited to each client's needs",
+    "Continuously improve company and client development methodologies by researching industry trends and best practices; conduct competitive analyses and internal reviews"
+
+]
+
+export default function BBGG() {
+    return (
+        <WorkPage
+            title={"BBG&G Integrated Marketing"}>
+
+            <Summary
+                summary={summary}
+                toolsList={toolsList}
+                imgInfo={imgInfo}
+                hasLink={false}
+            />
+
+            <Overview
+                overview={overview}
+                responsibilities={responsibilities}
+            />
+
+            <WorkSection>
+                <div class="mb-4 text-xl">
+                    <h2 class="text-3xl py-4 font-semibold">Website Builds</h2>
+                </div>
+
+                {/* row - paragraph */}
+                <WorkSection>
+                    <p class="mb-10">I collaborated with our Creative Director to craft two distinct websites for The Women&apos;s Business Enterprise Council of Metro NY and Greater DMV (WBEC Metro NY & Greater DMV)
+                        and the Capitol Region Council of Governments (CRCOG). I was responsible for transforming the Creative Director&apos;s wireframes into fully functional web pages while building all other sections of both websites.</p>
+                </WorkSection>
+
+                {/* row - image left + text right */}
+                <WorkRow>
+                    <div class="lg:w-2/3">
+                        <Image class="" src={summaryPhoto} alt="Four different user personas" placeholder="blur" width={1280} height={720} />
+
+                    </div>
+
+                    <div class="lg:w-1/3 lg:mb-10 lg:self-center">
+                        <h3 class="text-2xl py-3 font-semibold">WBEC Metro NY & Greater DMV</h3>
+                        <p>WBEC NY DMV&apos;s site was in need of a refresh, aligning with their recent rebranding, so we developed the new website to have a modern look, easier navigation, and clearer calls to action.</p>
+                    </div>
+                </WorkRow>
+
+                {/* row - pull quote - style more later */}
+                <WorkSection>
+                    <p class="mb-10">
+                        In tandem with implementing a new social media campaign, this project resulted in an 841.5% increase in new website users.
+                    </p>
+                </WorkSection>
+
+
+                {/* row - text left + image right */}
+                <WorkRow>
+                    <div class="lg:w-1/3 lg:mb-10 lg:self-center">
+                        <h3 class="text-2xl py-3 font-semibold">Capital Region Council of Governments</h3>
+                        <p class="mb-5">CRCOG staff sought a solution to help its target audiences effectively and efficiently access relevant and current information.</p>
+                        <p>We developed a website designed to accomplish enhanced organization, increase CRCOG&apos;s social media presence, and support its marketing efforts.</p>
+                    </div>
+
+                    <div class="lg:w-2/3">
+                        <Image class="" src={summaryPhoto} alt="Four different user personas" placeholder="blur" width={1280} height={720} />
+
+                    </div>
+                </WorkRow>
+
+                {/* row - pull quote - style more later */}
+                <WorkSection>
+                    <p class="mb-10">
+                        The new CRCOG website won a Gold Hermes Creative Award, evaluating the creative industry&apos;s best marketing and communication programs, in the category of Overall Website &ndash; Government.
+                    </p>
+                </WorkSection>
+            </WorkSection>
+
+
+            <WorkSection>
+                <div class="mb-4 text-xl">
+                    <h2 class="text-3xl py-4 font-semibold">UI/UX Design</h2>
+                </div>
+
+                {/* row - paragraph */}
+                <WorkSection>
+                    <h3 class="text-2xl py-3 font-semibold">BBG&G 25th Anniversary</h3>
+                    <p class="mb-10">I designed the homepage header and landing page for BBG&G Integrated Marketing&apos;s 25th anniversary. The header replaced the website&apos;s usual design, offering a preview of BBG&G&apos;s growth
+                        and success over the past 25 years. The landing page continued this narrative with an animated timeline showcasing more of the company&apos;s successes.</p>
+                </WorkSection>
+
+                <WorkSection>
+                    <h3 class="text-2xl py-3 font-semibold">BBG&G Case Studies</h3>
+                    <p class="mb-10">With the goal of achieving a cohesive and contemporary look that aligned seamlessly with the BBG&G's existing brand image, I designed new templates for the case studies showcased on the site.</p>
+                </WorkSection>
+            </WorkSection>
+        </WorkPage>
+    );
+}
