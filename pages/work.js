520da27784cca192e94acd2a821a47ceedbb18e2
Amanda Escaba <amandaescaba@gmail.com>
2024-04-07 14:09:17 -0400

    remove mbta university marketing project, edit wbec project page

--- pages/work.js
+++ pages/work.js
@@ -3,7 +3,7 @@ import WorkListing from '../components/worklisting'
 import Image from 'next/image'
 
 // image imports
-import wbec from '../public/images/work/wbecnydmv/wbec-thumb.jpg'
+import wbec from '../public/images/work/wbecnydmv/WBEC_preview_1.jpg'
 import capstone from '../public/images/work/capstone/capstone-thumb.jpg'
 import unimaps from '../public/images/work/mbta-unimaps/mbta-unimaps-thumb.jpg'
 import bluebook from '../public/images/work/mbta-bluebook/mbta-bluebook-thumb.jpg'
@@ -30,19 +30,14 @@ export default function Work() {
         </WorkListing>
 
         {/* project two */}
+
+
+        {/* project three */}
         <WorkListing
           imgsrc={capstone}
           path={"capstone"}
           title={"Tastes of the Motherland"}
           description={"Northeastern University capstone project: an interactive website prototype"}>
-        </WorkListing>
-
-        {/* project three */}
-        <WorkListing
-          imgsrc={unimaps}
-          path={"unimaps"}
-          title={"MBTA University Marketing: Interest-Based Maps"}
-          description={"Outreach plan to promote MBTA ridership to university students and young professionals"}>
         </WorkListing>
 
         {/* project four */}
@@ -55,7 +50,7 @@ export default function Work() {
 
       </div>
 
-      <h2 class="text-2xl mb-12 font-medium">In Progress &mdash; Coming Soon!</h2>
+      <h2 class="text-2xl mb-12 font-medium">In Progress</h2>
 
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-14 lg:gap-24 pb-24">
         {/* portfolio preview */}
--- pages/work/wbecnydmv.js
+++ pages/work/wbecnydmv.js
@@ -1,11 +1,17 @@
+// library imports
+import Image from 'next/image';
+
+// component imports
 import WorkPage from '../../components/work/workpage';
 import Summary from '../../components/work/summary';
 import Overview from '../../components/work/overview';
+// import WorkRow from '../../components/work/workrow';
 
 // image imports
-import summaryPhoto from '../../public/images/work/wbecnydmv/WBEC-3-crop-2.png'
+import summaryPhoto from '../../public/images/work/wbecnydmv/WBEC_preview_1.jpg'
+import responsiveMockup from '../../public/images/work/wbecnydmv/WBEC_responsive_mockup.png'
 
-const summary = "I love taking a design concept and seeing how far I can run with it, so designing and building website pages for my marketing agency’s client was a challenge I was excited to take on."
+const summary = "Amidst restructuring and rebranding, WBEC Metro NY and Greater DMV needed a website redesign to blend their new brand identity into a modern and cohesive online presence."
 
 const toolsList = [
   "WordPress",
@@ -22,13 +28,15 @@ const imgInfo = {
 }
 
 const overview = [
-  "Coming off the heels of a successful rebranding, the Women’s Business Enterprise Council of Metro NY and Greater DMV (WBEC NY DMV) wanted to update their website to reflect their fresh new look and feel. After my team’s creative director developed the new sitemap and a couple of wireframes, I designed the remaining 90+ pages based off of these concepts and handled the new website build. The result was a website with a modern look, easier navigation, and clearer calls to action."
+  "Following a successful rebranding initiative, the Women's Business Enterprise Council of Metro NY and Greater DMV (WBEC NY DMV) sought to align their website with their updated brand identity. With the direction provided by our team's creative director, I translated the new sitemap and initial wireframes into a cohesive design for over 90 pages of the website.",
+  "From conceptualization to execution, I oversaw the entire redesign process, ensuring a modern aesthetic, improved navigation, and more compelling calls to action. The result was a website that not only reflected the organization's fresh look but also enhanced user experience and engagement."
 ]
 
 const responsibilities = [
   "Designed and built 90+ responsive web pages based off old content and new requests, adhering to brand guidelines and 2 initial page wireframes",
   "Organized and coordinated technical and design process efforts between myself, creative director, and account staff/client liaison",
-  "Researched, tested, and implemented technical solutions to specific client requests"
+  "Researched, tested, and implemented technical solutions to specific client requests",
+  "Perform routine web maintenance: plugin updates, content updates, SEO audits"
 ]
 
 export default function WBECNYDMV() {
@@ -40,13 +48,20 @@ export default function WBECNYDMV() {
         summary={summary}
         toolsList={toolsList}
         imgInfo={imgInfo}
-        hasLink={false}
+        hasLink={true}
+        link="https://www.wbecnydmv.org/"
+        linkText="Visit Website"
       />
 
       <Overview
         overview={overview}
         responsibilities={responsibilities}
       />
+
+      {/* row - full size image */}
+      {/* <WorkRow> */}
+        <Image class="" src={responsiveMockup} alt="Early sketches of possible interactions; collection references and inspiration" placeholder="blur" width={1260} height={840} />
+      {/* </WorkRow> */}
     </WorkPage>
   );
 }
