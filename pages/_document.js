f4841f7d3157cbdfca0323c1eeb6b0a3a812c406
Amanda Escaba <amandaescaba@gmail.com>
2025-11-16 02:15:30 -0500

    remove dark mode and edit colors; index styling

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ import Document, {
   
     render() {
       return (
-        <Html class="dark bg-[#131315] text-[#F3F3F3]">
+        <Html class="bg-[#FFFBF3] text-[#131315]">
           <Head>
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
           </Head>
