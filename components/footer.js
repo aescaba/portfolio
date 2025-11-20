7dce3037b29e57f087bba2c333a16cffb8ca7d55
Amanda Escaba <amandaescaba@gmail.com>
2025-11-20 02:14:01 -0500

    navbar and footer styling

--- components/footer.js
+++ components/footer.js
@@ -7,13 +7,13 @@ export default function Footer() {
   return (
     <div className="relative py-20">
       <div className="absolute inset-x-0 bottom-0">
-        <div className="flex flex-wrap space-y-2 justify-between items-center text-sm text-stone-500">
-          <div className="">
-            <p>designed & developed ✩‧₊˚</p>
+        <div className="flex flex-wrap space-y-4 sm:space-y-0 justify-center md:justify-between items-center text-basis sm:text-sm text-stone-500">
+          <div className="flex">
+            <p>designed & developed ♡</p>
             </div>
           <div className="flex space-x-8">
-            <a href="mailto:amandaescaba@gmail.com">email</a>
-            <a href="https://www.linkedin.com/in/aescaba/" target="_blank">linkedin</a>
+            <div className="hover:text-blueberry"><a href="mailto:amandaescaba@gmail.com">email</a></div>
+            <div className="hover:text-blueberry"><a href="https://www.linkedin.com/in/aescaba/" target="_blank">linkedin</a></div>
           </div>
         </div>
         {/* <a className=""
