d21651ca131bab4335570f65b1356f3aa89ac3a6
Amanda Escaba <amandaescaba@gmail.com>
2025-11-20 01:20:06 -0500

    style and edit footer

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,19 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Footer() {
-    return (
-        <div className="relative py-10">
-        <div className="absolute inset-x-0 bottom-0">
-          <a className=""
+  return (
+    <div className="relative py-20">
+      <div className="absolute inset-x-0 bottom-0">
+        <div className="flex flex-wrap space-y-2 justify-between items-center text-sm text-stone-500">
+          <div className="">
+            <p>designed & developed ✩‧₊˚</p>
+            </div>
+          <div className="flex space-x-8">
+            <a href="mailto:amandaescaba@gmail.com">email</a>
+            <a href="https://www.linkedin.com/in/aescaba/" target="_blank">linkedin</a>
+          </div>
+        </div>
+        {/* <a className=""
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
@@ -16,8 +25,8 @@ export default function Footer() {
         <span className={styles.logo}>
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
-      </a>
+      </a> */}
       </div>
-      </div>
-    );
+    </div>
+  );
 }
