7d384a986abea9cfd84a7fe65bede7cc21fd7b2f
Amanda Escaba <amandaescaba@gmail.com>
2025-11-21 10:05:56 -0500

    footer styling

--- components/footer.js
+++ components/footer.js
@@ -7,11 +7,12 @@ export default function Footer() {
   return (
     <div className="relative mt-[10vh] py-20">
       <div className="absolute inset-x-0 bottom-0">
-        <div className="flex flex-wrap space-y-4 sm:space-y-0 md:justify-between items-center text-basis sm:text-sm">
+        <div className="flex flex-wrap space-y-4 sm:space-y-0 md:justify-between items-center text- sm:text-xs uppercase">
           <div className="flex">
             <p>designed & developed with next.js and tailwind</p>
           </div>
           <div className="flex space-x-8 uppercase">
+            <p className="font-bold">Contact</p>
             <div className="transition ease-in-out delay-75 hover:text-blueberry"><a href="mailto:amandaescaba@gmail.com">email &gt; </a></div>
             <div className="transition ease-in-out delay-75 hover:text-blueberry"><a href="https://www.linkedin.com/in/aescaba/" target="_blank">linkedin &gt;</a></div>
           </div>
