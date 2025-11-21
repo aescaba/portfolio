2bea89b5e0a0efb30756e41e59a10ce6247dcedb
Amanda Escaba <amandaescaba@gmail.com>
2025-11-21 00:32:53 -0500

    update home blurb, bio

--- pages/about.js
+++ pages/about.js
@@ -15,15 +15,15 @@ export default function About() {
         </div>
 
         {/* second column - bio */}
-        <div class="space-y-6 font-light text-lg/7">
+        <div class="space-y-6 font-light">
           <p class="font-semibold text-2xl">
          I&apos;m a designer and developer who thrives where creativity meets problem-solving.</p>
 
-          <p>I love coding for its logic and challenge, and design for its ability to tell stories and improve how people experience the web. Together, they let me create solutions that are both functional and human.</p>
+          <p>I love <span className="font-semibold">coding</span> for its logic and challenge and I love <span className="font-semibold">design</span> for its power to tell stories and make an impact. Together, they let me craft solutions that are both functional and human.</p>
 
-          <p>From designing UIs to building responsive sites to streamlining workflows, my process is grounded in curiosity, collaboration, and care for the people I&apos;m creating for and the people I work with.</p>
+          <p>The creative journey&mdash;<span className="font-semibold">untangling complexity, turning chaos into clarity, and bringing ideas to life</span>&mdash;energizes me, while my process is grounded in curiosity, collaboration, and care for the people I&apos;m creating for and the teams I work with.</p>
 
-          <p>I&apos;m driven by a passion for community and storytelling, creating spaces that foster connection, honor diverse voices, and help people feel seen.</p>
+          <p>From designing UIs to building responsive sites to streamlining workflows, I&apos;m driven by a passion for <span className="font-semibold">community and storytelling,</span> creating spaces that foster connection, honor diverse voices, and <span className="font-semibold">help people feel seen.</span></p>
 
           <p>Off the clock, you might find me catching up on my endless video game and book backlog, baking or cooking, curating too many Spotify playlists, or planning my next cosplay.</p>
 
