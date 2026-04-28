4a5a603f232d83ab653bb7139ac5618f6a9e326e
amanda <amandaescaba@gmail.com>
2026-04-28 18:25:29 -0400

    refactor metadata to use textblock component; sizing

--- components/textblock.js
+++ components/textblock.js
@@ -1,18 +1,23 @@
 
 
-export default function TextBlock({ isBulleted = false, heading = "", body = [] }) {
+export default function TextBlock({ numCols = 1, isBulleted = false, heading = "", body = [] }) {
 
     /*
+        numCols: int | number of columns in the row
         bulleted: boolean | is the body a bulleted list?
         heading: string | heading of the text block
         body: array (string) | body copy of the text block
+
+        Renders a text block comprised of a heading and body copy
+        Width is adjustable by the number of columns indicated
+        Displays the body copy in a bulleted list if indicated
     */
 
     return (
-        <div class="flex-col w-1/3">
+        <div class={"flex-col w-1/"+ `${numCols}` + " text-sm"}>
             <h3 class="uppercase text-base mb-2">{heading}</h3>
             {isBulleted ?
-                <ul className={"list-disc list-outside pl-4 text-sm"}>
+                <ul className="list-disc list-outside pl-4">
                     {body.map((bullet) => {
                         return (
                             <li key={bullet}>{bullet}</li>
--- components/work/workmetadata.js
+++ components/work/workmetadata.js
@@ -6,7 +6,7 @@
 import TextBlock from "../textblock";
 
 export default function WorkMetadata({ responsibilities, role, tools }) {
-    
+
     /*
         responsibilities: array (strings) | responsibilities on a project
         role: string | official role on a project
@@ -14,39 +14,28 @@ export default function WorkMetadata({ responsibilities, role, tools }) {
     */
 
     return (
-        <div class="flex-row mb-10">
+        <div class="flex flex-row gap-4 mb-10">
 
-<TextBlock
-isBulleted={true}
-heading="Responsibilities"
-body={responsibilities}
-/>
+            <TextBlock
+                numCols = "3"
+                isBulleted = {true}
+                heading = "Responsibilities"
+                body = {responsibilities}
+            />
 
-            <div class="flex-col w-1/3">
-                {/* <Heading heading={"Responsibilities"}/> */}
-                {/* {responsibilities.map((point) => {
-                        return (
-                            <li key={point}>{point}</li>
-                        );
-                    })} */}
-            </div>
-            <div class="flex-col w-1/3">
-                <h3 class="uppercase text-base">Role</h3>
-                <p>{role}</p>
-            </div>
+            <TextBlock
+                numCols = "3"
+                isBulleted = {false}
+                heading = "Role"
+                body = {role}
+            />
 
-            <div class="flex-col w-1/3">
-                <h3 class="uppercase text-base">Tools</h3>
-                <p>{tools}</p>
-            </div>
-
-            {/* <div class="mb-2 text-lg leading-6">
-                <h1 class=" font-bold">{title}</h1> 
-            </div>
-
-            <div class="mb-4 text-lg leading-8">
-                <h2 class="uppercase">{client}</h2>
-            </div> */}
+            <TextBlock
+                numCols = "3"
+                isBulleted = {false}
+                heading = "Tools"
+                body = {tools}
+            />
         </div>
     );
 }
