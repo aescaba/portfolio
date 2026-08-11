b9b768b5124ae225c85801259c68ad8ae1bd9e6d
Amanda Escaba <amandaescaba@gmail.com>
2026-08-11 00:42:36 -0400

    add background texture

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 import Layout from '../components/layout';
 import Navbar from '../components/navbar';
 import Head from 'next/head';
-import Footer from '../components/footer'
+import Footer from '../components/footer';
 
 
 function MyApp({ Component, pageProps }) {
