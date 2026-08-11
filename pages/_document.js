82826157c9abbc54f8d2de73638cb7ed50b2a3bf
Amanda Escaba <amandaescaba@gmail.com>
2026-08-11 00:47:17 -0400

    add background textures

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ import Document, {
   
     render() {
       return (
-        <Html class="bg-[#FFFBF3] text-[#131315]">
+        <Html class="bg-[#FFFBF3] text-[#131315] bg-[url(../public/images/cardboard_2.png)]" >
           <Head>
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"/>
           </Head>
